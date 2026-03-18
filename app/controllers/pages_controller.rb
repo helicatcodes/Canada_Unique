375bb6480eca0f3fb5ed267a340528637cbe508f
Helena Wali <helicatsventures@gmail.com>
2026-03-18 16:02:26 +0100

    added if statement to pages controller for departure date nil

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,12 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [:home]
 
   def home
   end
 
   def pre_canada
+    return unless current_user.departure_date.present?
+
     @countdown = (current_user.departure_date - Date.today).to_i
     # t1 = Time.current
     # t2 = current_user.departure_date
