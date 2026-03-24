f4223742085efe9102797b5afaf8f17f6d197f7c
Helena Wali <helicatsventures@gmail.com>
2026-03-24 16:11:26 +0100

    Unified with social feed & minimized upload placeholder

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -7,9 +7,9 @@ class PhotosController < ApplicationController
     # Check if the current user is allowed to upload photos. MJR
     authorize @photo
     if @photo.save
-      redirect_to in_canada_path, notice: "Photo uploaded!"
+      redirect_to in_canada_path(active_tab: "gallery"), notice: "Photo uploaded!"
     else
-      redirect_to in_canada_path, alert: "Upload failed."
+      redirect_to in_canada_path(active_tab: "gallery"), alert: "Upload failed."
     end
   end
 
